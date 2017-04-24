600d827b1a7f52e4bacedec2a66241ab194dc2cd
Sergio Saules <sergios@corp.globo.com>
2017-04-24 15:57:12 -0300

    Atualizei o icone de menu do chart

--- src/icon.js
+++ src/icon.js
@@ -9,12 +9,26 @@ import React from "react";
 export default class extends React.Component {
   render() {
     return (
-        <svg {...this.props} width="24" height="24" viewBox="0 0 24 24">
-          <g fill="none" fillRule="evenodd">
-          <path d="M0 0h24v24H0z"/>
-          <path d="M19.095 11.619h-1.143V8.571c0-.838-.685-1.523-1.523-1.523H13.38V5.905a1.905 1.905 0 0 0-3.81 0v1.143H6.524c-.838 0-1.516.685-1.516 1.523v2.896h1.135a2.058 2.058 0 0 1 0 4.114H5v2.895C5 19.314 5.686 20 6.524 20h2.895v-1.143a2.058 2.058 0 0 1 4.114 0V20h2.896c.838 0 1.523-.686 1.523-1.524V15.43h1.143a1.905 1.905 0 0 0 0-3.81z" fill="currentColor"/>
+      <svg {...this.props} width="24px" height="24px" viewBox="0 0 24 24">
+        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+          <g id="Desktop-HD">
+            <g id="Group-5">
+              <g id="Saiba-Mais-Copy-13">
+                <rect id="Rectangle-69-Copy-4" fill="#181818" x="0" y="0" width="24" height="24" rx="12"></rect>
+                <g id="video-icon-copy-16" transform="translate(3.000000, 3.000000)">
+                  <polygon id="Shape" points="0 0 18 0 18 18 0 18"></polygon>
+                </g>
+                <g id="ic_format_quote_24px-copy" transform="translate(3.000000, 3.000000)">
+                  <g id="Group">
+                    <polygon id="Shape" points="-2.66453526e-15 -1.33226763e-15 18 -1.33226763e-15 18 18 -2.66453526e-15 18"></polygon>
+                  </g>
+                </g>
+              </g>
+              <polygon id="Shape" fill="#FFFFFF" fillRule="nonzero" points="14.625 11.68875 17.01 7.565625 17.983125 8.128125 15.04125 13.21875 11.379375 11.109375 8.69625 15.75 18 15.75 18 16.875 6.75 16.875 6.75 6.75 7.875 6.75 7.875 14.92875 10.96875 9.5625"></polygon>
+            </g>
           </g>
-        </svg>
+        </g>
+      </svg>
     );
   }
 }
