e1274bd277fe0f5365dbe56b04e02e9d73736fda
Sergio Saules <sergios@corp.globo.com>
2017-05-10 14:12:18 -0300

    Mudanca no icone do grafico

--- src/icon.js
+++ src/icon.js
@@ -9,22 +9,11 @@ import React from "react";
 export class PluginIcon extends React.Component {
   render() {
     return (
-      <svg {...this.props} width="24" height="24" viewBox="0 0 24 24">
-        <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-          <g id="Desktop-HD">
-            <g id="Group-5">
-              <g id="Saiba-Mais-Copy-13">
-                <rect id="Rectangle-69-Copy-4" fill="none" x="0" y="0" width="24" height="24" rx="12"></rect>
-                <g id="video-icon-copy-16" transform="translate(3.000000, 3.000000)">
-                  <polygon id="Shape" points="0 0 18 0 18 18 0 18"></polygon>
-                </g>
-                <g id="ic_format_quote_24px-copy" transform="translate(3.000000, 3.000000)">
-                  <g id="Group">
-                    <polygon id="Shape" points="-2.66453526e-15 -1.33226763e-15 18 -1.33226763e-15 18 18 -2.66453526e-15 18"></polygon>
-                  </g>
-                </g>
-              </g>
-              <polygon id="Shape" fill="#FFFFFF" fillRule="nonzero" points="14.625 11.68875 17.01 7.565625 17.983125 8.128125 15.04125 13.21875 11.379375 11.109375 8.69625 15.75 18 15.75 18 16.875 6.75 16.875 6.75 6.75 7.875 6.75 7.875 14.92875 10.96875 9.5625"></polygon>
+      <svg {...this.props} width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="48" fill-rule="nonzero" fill="#FFFFFF">
+            <g id="icone-grafico" transform="translate(6.000000, 7.000000)">
+              <polygon id="Shape" points="7.875 4.93875 10.26 0.815625 11.233125 1.378125 8.29125 6.46875 4.629375 4.359375 1.94625 9 11.25 9 11.25 10.125 0 10.125 0 0 1.125 0 1.125 8.17875 4.21875 2.8125"></polygon>
             </g>
           </g>
         </g>
