9f77f80803f98127dcd7581c4d568db0007c5b69
Sergio Saules <sergios@corp.globo.com>
2017-05-24 18:10:52 -0300

    Aspas simples por dupla

--- src/ModalChart.js
+++ src/ModalChart.js
@@ -131,8 +131,8 @@ export default class ModalChart extends Component {
   _onSaveRequest = () => {
     let themes = this.model[this.chartType]["themes"];
     let options = this.model[this.chartType]["options"];
-    let headerSVG = "<?xml version='1.0' encoding='utf-8'?>" +
-      "<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>";
+    let headerSVG = '<?xml version="1.0" encoding="utf-8"?>' +
+      '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
     let svgData = headerSVG + this.chartComponent.chart.getSVG();
     svgData = this._safeTags(svgData);
 
